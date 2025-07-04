e20d4b22e65af125c0b37582896db8c8478abc53
Dhanish S <sdhanish92@gmail.com>
2025-07-05 00:40:05 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,40 @@ import notesRoutes from './routes/notesRoutes.js'
 import { connectDb } from "./config/db.js";
 import cors from 'cors'
 
+import path from 'path';
+
 
 const PORT = process.env.PORT || 5001
 
+const __dirname = path.resolve();
+
 
 app.use(express.json());
+
+
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
   origin:"http://localhost:5173",
 }))
+}
+
 // app.use((req,res,next)=>{
 //   console.log(`middleware ${req.method} ${req.url}`);
 // next();
   
 // })
 app.use('/api/notes',notesRoutes)
+
+
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+});
+}
 
 connectDb().then(()=>{
 app.listen(PORT,()=>{
